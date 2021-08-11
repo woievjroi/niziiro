09cf11344fe63185858f146466555392f81554f2
woievjroi <82654085+woievjroi@users.noreply.github.com>
2021-08-11 15:18:10 +0900

    Add files via upload

---
+++ 虹色の鉛筆募金/assist.js
@@ -0,0 +1,14 @@
+var _window = $(window),
+    _header = $('.page-header'),
+    heroBottom;
+
+_window.on('scroll', function() {
+    heroBottom = $('.hero').height();
+    if (_window.scrollTop() > heroBottom) {
+        _header.addClass('fixed');
+    } else {
+        _header.removeClass('fixed');
+    }
+});
+
+_window.trigger('scroll');
